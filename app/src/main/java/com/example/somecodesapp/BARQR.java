f2f3bf4822009516be1e83b68489e8d885cc09a1
Mateusz Janicki <mj4nicki@gmail.com>
2022-09-19 23:10:13 +0200

    Initial commit

---
+++ app/src/main/java/com/example/somecodesapp/BARQR.java
@@ -0,0 +1,13 @@
+package com.example.somecodesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BARQR extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barqr);
+    }
+}
